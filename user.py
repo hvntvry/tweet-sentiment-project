2685834662124afba9ece7994da9ae5a7a000cf7
hvntvry <hyoungquist@gmail.com>
2021-02-07 11:19:48 +0100

    Update user.py

--- user.py
+++ user.py
@@ -7,16 +7,13 @@ set_api accesses the API and sets a new attribute for the tweet class can access
 
 class User:
 
-    def __init__(self, consumer_key, consumer_secret, access_token, access_secret):
+    def __init__(self, consumer_key, consumer_secret):
         self.consumer_key = consumer_key
         self.consumer_secret = consumer_secret
-        self.access_token = access_token
-        self.access_secret = access_secret
         self.api = None
 
     # grants access to the twitter api by using the various keys, sets the attribute api for the tweet class to use
     def set_api(self):
         auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
-        auth.set_access_token(self.access_token, self.access_secret)
         self.api = tweepy.API(auth)
         
